673268e7d4bd275a11caabc55cc15bbf49a5570c
maxbalahurak <92430278+MaksymBohun2004@users.noreply.github.com>
2022-02-16 13:43:23 +0200

    Create hidden.py

---
+++ hidden.py
@@ -0,0 +1,13 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+# downloaded from https://www.py4e.com/code3/
+def oauth():
+    """
+    Enter Your keys here:
+    """
+    return {"consumer_key": "your_key",
+            "consumer_secret": "your_key",
+            "token_key": "your_key",
+            "token_secret": "your_key"}
